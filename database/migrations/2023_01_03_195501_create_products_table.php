8df8ce607214e98f16b754d1353a2caf814922d1
kodyfy-7 <ucheofunne.o@gmail.com>
2023-01-03 21:50:37 +0100

    setup database migrations

---
+++ database/migrations/2023_01_03_195501_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('category_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('
+                cascade');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->decimal('price', 10, 2)->nullable();            
+            $table->string('image')->nullable();
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2023_01_03_195519_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('transaction_id')->unique()->index()->default(DB::raw('uuid_generate_v4()'));
+            $table->integer('user_id');
+            $table->decimal('total', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
